656b26d01433f6575aaae4856c51f569e7411ea3
MathiasWitt <33960380+Mathias-Witt@users.noreply.github.com>
2018-05-08 12:00:13 +0200

    Update and rename asse-filter-upload.php to wp-mu-filter-upload.php

--- wp-mu-filter-upload.php
+++ wp-mu-filter-upload.php
@@ -2,7 +2,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-class Asse_Filter_Upload {
+class Axelspringer_Filter_Upload {
 
     public function __construct() {
         add_filter( 'upload_dir', array( $this, 'set_upload_dir' ) );
@@ -44,4 +44,4 @@ class Asse_Filter_Upload {
     }
 }
 
-$asse_upload = new Asse_Filter_Upload();
+$asse_upload = new Asse_Axelspringer_Upload();
